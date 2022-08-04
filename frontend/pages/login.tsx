140875f706683db2ab66cf74b761b98253eb0233
IvanMukticc <ivan.muktic@gmail.com>
2022-08-04 19:06:57 +0200

    Added screens for register

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -5,6 +5,7 @@ const Login: NextPage = () => {
   return (
     <div>
       <Input label="Username" />
+      <Input label="Password" />
     </div>
   );
 };
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -3,10 +3,22 @@ import { Button, Checkbox, Input } from "@krtech-digital/krtech-ui";
 
 const Register: NextPage = () => {
   return (
-    <div>
-         <Button buttonText="Hello Register" variant="primary"/>    
-    </div>
+    <div className="main.style">
+        <div className="content.style">
+        <Input label="FirstName" placeholder="Please enter first Name"/> 
+        <Input label="LastName" placeholder="Please enter last Name"/>
+        <Input label="Email" placeholder="Please enter e-mail"/>
+        <Input label="Password" placeholder="Please enter password"/> 
+        </div>
+         <Button buttonText="Submit" variant="primary"/>    
+    </div>   
+    
   );
+
+  
+  
 };
+
+
 
 export default Register;
