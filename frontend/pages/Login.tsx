1202f823803b125eb4580d1afbf35ef8ed93db4e
Vilko <vilko.poljak1@gmail.com>
2022-08-11 18:29:37 +0200

    Update

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import { Button, Checkbox, Input } from "@krtech-digital/krtech-ui";
+import style from "../styles/Login.module.css";
 
 const Login: NextPage = () => {
-  return <div>Hello Login</div>;
+  return (
+    <div>
+      <div className={style.Container}>
+        <Input label="Username"></Input>
+        <Input type="password" label="Password"></Input>
+        <Button
+          style={{ marginTop: "10px", marginLeft: "110px" }}
+          buttonText="Log in"
+          variant="primary"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
