f7433e427595729a1393009d9ae50c726673600f
kukovecd-krtech <104830337+kukovecd-krtech@users.noreply.github.com>
2022-08-11 17:37:32 +0200

    Delete login.tsx

--- frontend/pages/login.tsx
+++
@@ -1,8 +0,0 @@
-import { Input } from "@krtech-digital/krtech-ui";
-import type { NextPage } from "next";
-
-const Login: NextPage = () => {
-  return <div>Hello Login</div>;
-};
-
-export default Login;
