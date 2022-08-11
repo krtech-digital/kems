772728a562e5a9d891e698fb11f33d2b69db5165
admin <admin@admins-MBP.fritz.box>
2022-08-11 09:27:47 -0700

    Created register screen

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,7 +1,22 @@
+import styles from '../styles/register.module.css';
 import type { NextPage } from "next";
+import { Button} from "@krtech-digital/krtech-ui";
+import { Input } from "@krtech-digital/krtech-ui";
 
 const Register: NextPage = () => {
-  return <div>Hello Register</div>;
+  return (
+    <>
+    <div className={styles.test}>
+      <p>Sign Up</p>
+      <p>Want to Sign up fill out this form!</p>
+    <Input label="First name" placeholder='Jamie'/>
+    <Input label="Last name" placeholder="Oliver"/>
+    <Input label="Username" placeholder='Jamie123'/>
+    <Input label="Password" type="password"/>
+    <Button buttonText="Sign Up" variant="primary" style={{ margin: 25 }} />
+    </div>
+    </>
+  )
 };
 
 export default Register;
