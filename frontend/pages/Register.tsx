584baef0547f3dfde89759e57156263157f26946
kukovecd-krtech <104830337+kukovecd-krtech@users.noreply.github.com>
2022-08-11 17:37:05 +0200

    Update Register.tsx

--- frontend/pages/Register.tsx
+++ frontend/pages/Register.tsx
@@ -1,10 +1,23 @@
+
+
 import type { NextPage } from "next";
-import { Button, Checkbox } from "@krtech-digital/krtech-ui";
+import { Button, Checkbox, Input } from "@krtech-digital/krtech-ui";
+import style from "../styles/Register.module.css";
 
 const Register: NextPage = () => {
   return (
     <div>
-      <Button buttonText="Hello Register" variant="primary" />
+      <div className={style.Container}>
+        <Input label="First name"></Input>
+        <Input label="Last name"></Input>
+        <Input label="Username"></Input>
+        <Input type="password" label="Password"></Input>
+        <Button
+          style={{ marginTop: "10px", marginLeft: "110px" }}
+          buttonText="Sign up"
+          variant="primary"
+        />
+      </div>
     </div>
   );
 };
